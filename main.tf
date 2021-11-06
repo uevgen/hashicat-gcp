26445bc5d33870194a8b1518bfa2e5edc6036a4e
Yevgen <yevgen.berlyand@db.com>
2021-11-06 14:54:20 +0000

    Added the first label

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "tls_private_key" "ssh-key" {
   rsa_bits  = "4096"
 }
 
-resource "google_compute_instance" "hashicat" "department:devops" {
+resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
@@ -68,6 +68,7 @@ resource "google_compute_instance" "hashicat" "department:devops" {
 
   labels = {
     name = "hashicat"
+    department = "devops"
   }
 
 }
