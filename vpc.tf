73620ea341239f03612a88cfd0d7f7767d44cd7c
Yevgen <yevgen.berlyand@db.com>
2021-11-06 15:25:01 +0000

    Add module 1

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/Yevgen_org/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  project_id = var.project_id
+  network_name = "gaurav-network"
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
